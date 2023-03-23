b68149d0c90eca3762cc88d1a02e76c5efba91a6
Rafael Ferreira <122361848+rafaelFerreiraa@users.noreply.github.com>
2023-03-23 11:49:27 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+function buttonPress1() {
+let coffeeBtn1 = document.getElementById("coffee-Btn1");
+coffeeBtn1.innerHTML = "Coming Soon!";
+}
+
+function buttonPress2() {
+let coffeeBtn2 = document.getElementById("coffee-Btn2");
+coffeeBtn2.innerHTML = "Coming Soon!";
+} 
+
+function buttonPress3() {
+let coffeeBtn3 = document.getElementById("coffee-Btn3");
+coffeeBtn3.innerHTML = "Coming Soon!";
+}    
+
+function openNav() {
+
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+/* copyright by rafael */
+
